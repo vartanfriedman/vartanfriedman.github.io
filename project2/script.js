57c7421448ac7c2d3fc60f7eced1c636703baeef
alannarisse <angrypirate@gmail.com>
2022-11-30 13:38:58 -0800

    salads

--- project2/script.js
+++ project2/script.js
@@ -1,9 +1,13 @@
-$('#pizza').click(function() {
-    $('#pizza-menu').css('display', 'block')
-    $('article').not('#pizza-menu').css('display', 'none')
+$('#pizza').click(function () {
+  $('#pizza-menu').css('display', 'block')
+  $('article').not('#pizza-menu').css('display', 'none')
 })
-$('#snacks').click(function() {
-    $('#snacks-menu').css('display', 'block')
-    $('article').not('#snacks-menu').css('display', 'none')
+$('#snacks').click(function () {
+  $('#snacks-menu').css('display', 'block')
+  $('article').not('#snacks-menu').css('display', 'none')
 })
 
+$('#salads').click(function () {
+  $('#salads-menu').css('display', 'block')
+  $('article').not('#salads-menu').css('display', 'none')
+})
