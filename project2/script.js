420a7dc4f7f5bb658080395bf143a99497f11be4
vartanfriedman <vcf@pdx.edu>
2022-12-03 15:04:19 -0800

    project 2 work

--- project2/script.js
+++ project2/script.js
@@ -6,4 +6,15 @@ $('#snacks').click(function() {
     $('#snacks-menu').css('display', 'block')
     $('article').not('#snacks-menu').css('display', 'none')
 })
-
+$('#salads').click(function() {
+    $('#salads-menu').css('display', 'block')
+    $('article').not('#salads-menu').css('display', 'none')
+})
+$('#sweets').click(function() {
+    $('#sweets-menu').css('display', 'block')
+    $('article').not('#sweets-menu').css('display', 'none')
+})
+$('#drinks').click(function() {
+    $('#drinks-menu').css('display', 'block')
+    $('article').not('#drinks-menu').css('display', 'none')
+})
